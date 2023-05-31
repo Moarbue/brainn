698eca3a36db619a5672d8fdb9680a7ac4cf1175
Moarbue <83272310+Moarbue@users.noreply.github.com>
2023-05-31 17:44:42 +0200

    Add activations.c to sources

--- build.sh
+++ build.sh
@@ -5,10 +5,11 @@ CFLAGS="-O3 -Wall -Wextra"
 set -xe
 
 mkdir -p lib
-gcc $CFLAGS -c src/vec.c -o lib/vec.o
-gcc $CFLAGS -c src/mat.c -o lib/mat.o
-gcc $CFLAGS -c src/nn.c  -o lib/nn.o
+gcc $CFLAGS -c src/vec.c         -o lib/vec.o
+gcc $CFLAGS -c src/mat.c         -o lib/mat.o
+gcc $CFLAGS -c src/activation.c  -o lib/activation.o
+gcc $CFLAGS -c src/nn.c          -o lib/nn.o
 
-ar -rcs lib/brainn.a lib/vec.o lib/mat.o lib/nn.o
+ar -rcs lib/libbrainn.a lib/vec.o lib/mat.o lib/activation.o lib/nn.o
 
 rm -rf lib/vec.o lib/mat.o lib/nn.o
