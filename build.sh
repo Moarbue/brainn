0c4a539dbfb6192f574ff8c0049c001e1cbb75ac
Moarbue <83272310+Moarbue@users.noreply.github.com>
2023-05-31 13:31:37 +0200

    Add simple build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+CFLAGS="-O3 -Wall -Wextra"
+
+set -xe
+
+mkdir -p lib
+gcc $CFLAGS -c src/vec.c -o lib/vec.o
+gcc $CFLAGS -c src/mat.c -o lib/mat.o
+gcc $CFLAGS -c src/nn.c  -o lib/nn.o
+
+ar -rcs lib/brainn.a lib/vec.o lib/mat.o lib/nn.o
+
+rm -rf lib/vec.o lib/mat.o lib/nn.o
