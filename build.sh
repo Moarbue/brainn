da507c7ca6526fcb72c282c4968e70342196e5cf
Moarbue <83272310+Moarbue@users.noreply.github.com>
2023-06-01 11:15:42 +0200

    Add missing sources

--- build.sh
+++ build.sh
@@ -7,9 +7,11 @@ set -xe
 mkdir -p lib
 gcc $CFLAGS -c src/vec.c         -o lib/vec.o
 gcc $CFLAGS -c src/mat.c         -o lib/mat.o
+gcc $CFLAGS -c src/vec_mat.c     -o lib/vec_mat.o
 gcc $CFLAGS -c src/activation.c  -o lib/activation.o
+gcc $CFLAGS -c src/loss.c        -o lib/loss.o
 gcc $CFLAGS -c src/nn.c          -o lib/nn.o
 
-ar -rcs lib/libbrainn.a lib/vec.o lib/mat.o lib/activation.o lib/nn.o
+ar -rcs lib/libbrainn.a lib/vec.o lib/mat.o lib/vec_mat.o lib/activation.o lib/loss.o lib/nn.o
 
-rm -rf lib/vec.o lib/mat.o lib/nn.o
+rm -rf lib/vec.o lib/mat.o lib/vec_mat.o lib/activation.o lib/loss.o lib/nn.o
