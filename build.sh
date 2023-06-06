5f1e18d792d462c44371e9eac12a23001e6378fd
Moarbue <83272310+Moarbue@users.noreply.github.com>
2023-06-06 10:11:58 +0200

    Add optimizer.c to sources

--- build.sh
+++ build.sh
@@ -11,7 +11,8 @@ gcc $CFLAGS -c src/vec_mat.c     -o lib/vec_mat.o
 gcc $CFLAGS -c src/activation.c  -o lib/activation.o
 gcc $CFLAGS -c src/loss.c        -o lib/loss.o
 gcc $CFLAGS -c src/nn.c          -o lib/nn.o
+gcc $CFLAGS -c src/optimizer.c   -o lib/optimizer.o
 
-ar -rcs lib/libbrainn.a lib/vec.o lib/mat.o lib/vec_mat.o lib/activation.o lib/loss.o lib/nn.o
+ar -rcs lib/libbrainn.a lib/vec.o lib/mat.o lib/vec_mat.o lib/activation.o lib/loss.o lib/nn.o lib/optimizer.o
 
 rm -rf lib/vec.o lib/mat.o lib/vec_mat.o lib/activation.o lib/loss.o lib/nn.o
