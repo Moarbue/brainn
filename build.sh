69fbb0af099ab605acb85ed425bc2b3c83ce5edd
Moarbue <83272310+Moarbue@users.noreply.github.com>
2023-11-23 11:19:03 +0100

    Ready. Set. Go!

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+DEBUG=true
+
+AR=ar
+AR_FLAGS=-rcs
+CC=gcc
+CFLAGS_DEBUG="-Wall -Wextra -O0 -ggdb -g"
+CFLAGS_RELEASE="-Wall -Wextra -O3"
+CFLAGS=""
+
+LIB_NAME=libbrainn.a
+LIB_PATH=lib
+BIN_PATH=bin
+LIBS=-lm -Llib -lbrainn
+
+if [ "$DEBUG" = true ] ; then   CFLAGS=$CFLAGS_DEBUG
+else    CFLAGS=$CFLAGS_RELEASE
+fi
+
+set -xe
+
+mkdir -p $LIB_PATH
+mkdir -p $BIN_PATH
+
+$CC $CFLAGS -c -o $LIB_PATH/mat.o src/mat.c
+$AR -rcs $LIB_PATH/$LIB_NAME $LIB_PATH/mat.o
+rm -rf $LIB_PATH/mat.o
+
+$CC $CFLAGS -o $BIN_PATH/logic_gates examples/logic_gates.c $LIBS
---
+++ examples/logic_gates.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello Neural Network!\n");
+
+    return 0;
+}
