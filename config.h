12e7ed9a2057170eb744cb913a2d2f318b3a5bcd
Moarbue <83272310+Moarbue@users.noreply.github.com>
2023-11-23 12:00:48 +0100

    Add general configuration file

---
+++ config.h
@@ -0,0 +1,29 @@
+// General configuration for the library
+
+#ifndef _BRAINN_CONFIG_H
+#define _BRAINN_CONFIG_H
+
+// for size_t
+#include <inttypes.h>
+// rand() and malloc()
+#include <stdlib.h>
+// printf()
+#include <stdio.h>
+
+#define BALLOC malloc
+#define BFREE  free
+
+#define PANIC(msg) do { printf(msg); exit(EXIT_FAILURE); } while(0)
+
+// type of floating precision number
+typedef float bfloat;
+// type of unsigned integer
+typedef size_t bsize;
+
+// random function
+static inline bfloat brand(bfloat min, bfloat max)
+{
+    return ((bfloat) rand() / (bfloat) RAND_MAX) * (max - min) + min;
+}
+
+#endif // _BRAINN_CONFIG_H
