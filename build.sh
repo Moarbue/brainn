1ae9a458181a93cfcefbec9bd67175092dd55c8b
Moarbue <83272310+Moarbue@users.noreply.github.com>
2023-11-23 12:50:29 +0100

    Update sources

--- build.sh
+++ build.sh
@@ -25,9 +25,10 @@ mkdir -p $BIN_PATH
 
 $CC $CFLAGS -c -o $LIB_PATH/mat.o src/mat.c
 $CC $CFLAGS -c -o $LIB_PATH/vec.o src/vec.c
+$CC $CFLAGS -c -o $LIB_PATH/vec_mat.o src/vec_mat.c
 
-$AR -rcs $LIB_PATH/$LIB_NAME $LIB_PATH/mat.o $LIB_PATH/vec.o
+$AR -rcs $LIB_PATH/$LIB_NAME $LIB_PATH/mat.o $LIB_PATH/vec.o $LIB_PATH/vec_mat.o
 
-rm -rf $LIB_PATH/mat.o $LIB_PATH/vec.o
+rm -rf $LIB_PATH/mat.o $LIB_PATH/vec.o $LIB_PATH/vec_mat.o
 
 $CC $CFLAGS -o $BIN_PATH/logic_gates examples/logic_gates.c $LIBS
