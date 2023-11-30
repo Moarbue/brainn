d049195bacbe3cc6dd66a53d266d20ae6528465d
Moarbue <83272310+Moarbue@users.noreply.github.com>
2023-11-30 16:04:41 +0100

    Implement vec_to_mat_row() and vec_to_mat_col()

--- include/vec_mat.h
+++ include/vec_mat.h
@@ -8,5 +8,7 @@
 void vec_mat_mul(Vec dst, Vec v, Mat m);
 Vec  mat_to_row_vec(Mat src, bsize r);
 Vec  mat_to_col_vec(Mat src, bsize c);
+void vec_to_mat_row(Mat dst, Vec v, bsize r);
+void vec_to_mat_col(Mat dst, Vec v, bsize c);
 
 #endif // _BRAINN_VEC_MAT_H_
--- src/vec_mat.c
+++ src/vec_mat.c
@@ -25,7 +25,7 @@ Vec  mat_to_row_vec(Mat src, bsize r)
     return dst;
 }
 
-Vec  mat_to_col_vec(Mat src, bsize c)
+Vec mat_to_col_vec(Mat src, bsize c)
 {
     if (c >= src.c) PANIC("mat_to_col_vec(): col index is out of bounds");
 
@@ -36,4 +36,24 @@ Vec  mat_to_col_vec(Mat src, bsize c)
     dst.e = &mat_el(src, 0, c);
 
     return dst;
+}
+
+void vec_to_mat_row(Mat dst, Vec v, bsize r)
+{
+    if (r >= dst.r)   PANIC("vec_to_mat_row(): row index is out of bounds");
+    if (v.c != dst.c) PANIC("vec_to_mat_row(): destination size differs from vec size");
+
+    for (bsize c = 0; c < v.c; c++) {
+        mat_el(dst, r, c) = vec_el(v, c);
+    } 
+}
+
+void vec_to_mat_col(Mat dst, Vec v, bsize c)
+{
+    if (c >= dst.c)   PANIC("vec_to_mat_col(): col index is out of bounds");
+    if (v.c != dst.r) PANIC("vec_to_mat_col(): destination size differs from vec size");
+
+    for (bsize r = 0; r < v.c; r++) {
+        mat_el(dst, r, c) = vec_el(v, r);
+    } 
 }
