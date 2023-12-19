666b91a02cd89e10ac7274339d97e6565657ed18
Moarbue <83272310+Moarbue@users.noreply.github.com>
2023-12-19 20:28:20 +0100

    Move config functions to seperate file

--- src/nn.c
+++ src/nn.c
@@ -197,36 +197,6 @@ void nn_free(NN nn)
     BFREE(nn.gc);
 }
 
-void nn_set_activation_function(NN *nn, hidden_activation hf, dhidden_activation dhf, 
-                                        output_activation of, doutput_activation dof)
-{
-    nn->hf  = hf;
-    nn->dhf = dhf;
-    nn->of  = of;
-    nn->dof = dof;
-}
-
-void nn_set_loss_function(NN *nn, loss_function C, dloss_function dC)
-{
-    nn->C  = C;
-    nn->dC = dC;
-}
-
-void nn_get_arch(NN nn, bsize *arch[], bsize *layers)
-{
-    bsize *a, L;
-
-    L = nn.l + 1;
-    a = BALLOC(L * sizeof (bsize));
-
-    for (bsize l = 0; l < L; l++) {
-        a[l] = nn.a[l].c;
-    }
-
-    *arch   = a;
-    *layers = L;
-}
-
 
 void nn_print_intern(NN nn, const char *name)
 {
---
+++ src/nn_config.c
@@ -0,0 +1,31 @@
+#include "../include/nn.h"
+
+void nn_set_activation_function(NN *nn, hidden_activation hf, dhidden_activation dhf, 
+                                        output_activation of, doutput_activation dof)
+{
+    nn->hf  = hf;
+    nn->dhf = dhf;
+    nn->of  = of;
+    nn->dof = dof;
+}
+
+void nn_set_loss_function(NN *nn, loss_function C, dloss_function dC)
+{
+    nn->C  = C;
+    nn->dC = dC;
+}
+
+void nn_get_arch(NN nn, bsize *arch[], bsize *layers)
+{
+    bsize *a, L;
+
+    L = nn.l + 1;
+    a = BALLOC(L * sizeof (bsize));
+
+    for (bsize l = 0; l < L; l++) {
+        a[l] = nn.a[l].c;
+    }
+
+    *arch   = a;
+    *layers = L;
+}
