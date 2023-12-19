3264b61db47f41e4ad9ffa6f4d8300fb628396b4
Moarbue <83272310+Moarbue@users.noreply.github.com>
2023-12-19 11:34:19 +0100

    Update sources

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ CFLAGS=""
 LIB_NAME=libbrainn.a
 LIB_PATH=lib
 BIN_PATH=bin
-LIBS="-lm -Llib -lbrainn"
+LIBS="-lm -lpthread -Llib -lbrainn"
 
 if [ "$DEBUG" = true ] ; then   CFLAGS=$CFLAGS_DEBUG
 else    CFLAGS=$CFLAGS_RELEASE
@@ -30,10 +30,11 @@ $CC $CFLAGS -c -o $LIB_PATH/vec_mat.o src/vec_mat.c
 $CC $CFLAGS -c -o $LIB_PATH/nn.o src/nn.c
 $CC $CFLAGS -c -o $LIB_PATH/loss.o src/loss.c
 $CC $CFLAGS -c -o $LIB_PATH/activation.o src/activation.c
+$CC $CFLAGS -c -o $LIB_PATH/nn_parallel.o src/nn_parallel.c
 
-$AR -rcs $LIB_PATH/$LIB_NAME $LIB_PATH/mat.o $LIB_PATH/vec.o $LIB_PATH/vec_mat.o $LIB_PATH/nn.o $LIB_PATH/loss.o $LIB_PATH/activation.o
+$AR -rcs $LIB_PATH/$LIB_NAME $LIB_PATH/mat.o $LIB_PATH/vec.o $LIB_PATH/vec_mat.o $LIB_PATH/nn.o $LIB_PATH/loss.o $LIB_PATH/activation.o $LIB_PATH/nn_parallel.o
 
-rm -rf $LIB_PATH/mat.o $LIB_PATH/vec.o $LIB_PATH/vec_mat.o $LIB_PATH/nn.o $LIB_PATH/loss.o $LIB_PATH/activation.o
+rm -rf $LIB_PATH/mat.o $LIB_PATH/vec.o $LIB_PATH/vec_mat.o $LIB_PATH/nn.o $LIB_PATH/loss.o $LIB_PATH/activation.o $LIB_PATH/nn_parallel.o
 
 $CC $CFLAGS -o $BIN_PATH/logic_gates examples/logic_gates.c $LIBS
 $CC $CFLAGS -o $BIN_PATH/img_compression examples/img_compression/img_compression.c $LIBS
