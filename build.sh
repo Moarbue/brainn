9544569de50d902be3248f763cd92ec38bdd6d50
Moarbue <83272310+Moarbue@users.noreply.github.com>
2023-12-19 20:23:59 +0100

    Update sources

--- build.sh
+++ build.sh
@@ -32,10 +32,12 @@ $CC $CFLAGS -c -o $LIB_PATH/loss.o src/loss.c
 $CC $CFLAGS -c -o $LIB_PATH/activation.o src/activation.c
 $CC $CFLAGS -c -o $LIB_PATH/nn_parallel.o src/nn_parallel.c
 $CC $CFLAGS -c -o $LIB_PATH/nn_io.o src/nn_io.c
+$CC $CFLAGS -c -o $LIB_PATH/buffer.o src/buffer.c
+$CC $CFLAGS -c -o $LIB_PATH/serialization.o src/serialization.c
 
-$AR -rcs $LIB_PATH/$LIB_NAME $LIB_PATH/mat.o $LIB_PATH/vec.o $LIB_PATH/vec_mat.o $LIB_PATH/nn.o $LIB_PATH/loss.o $LIB_PATH/activation.o $LIB_PATH/nn_parallel.o $LIB_PATH/nn_io.o
+$AR -rcs $LIB_PATH/$LIB_NAME $LIB_PATH/mat.o $LIB_PATH/vec.o $LIB_PATH/vec_mat.o $LIB_PATH/nn.o $LIB_PATH/loss.o $LIB_PATH/activation.o $LIB_PATH/nn_parallel.o $LIB_PATH/nn_io.o $LIB_PATH/buffer.o $LIB_PATH/serialization.o
 
-rm -rf $LIB_PATH/mat.o $LIB_PATH/vec.o $LIB_PATH/vec_mat.o $LIB_PATH/nn.o $LIB_PATH/loss.o $LIB_PATH/activation.o $LIB_PATH/nn_parallel.o $LIB_PATH/nn_io.o
+rm -rf $LIB_PATH/mat.o $LIB_PATH/vec.o $LIB_PATH/vec_mat.o $LIB_PATH/nn.o $LIB_PATH/loss.o $LIB_PATH/activation.o $LIB_PATH/nn_parallel.o $LIB_PATH/nn_io.o $LIB_PATH/buffer.o $LIB_PATH/serialization.o
 
 $CC $CFLAGS -o $BIN_PATH/logic_gates examples/logic_gates.c $LIBS
 $CC $CFLAGS -o $BIN_PATH/img_compression examples/img_compression/img_compression.c $LIBS
