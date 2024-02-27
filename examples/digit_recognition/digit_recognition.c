1ae9220ca59f325febb6cc5b56c34639f6a0756a
Moarbue <tomkaufmann1206@gmail.com>
2024-02-27 18:10:32 +0100

    Save network after training

--- examples/digit_recognition/digit_recognition.c
+++ examples/digit_recognition/digit_recognition.c
@@ -9,20 +9,27 @@ void print_img(Vec img, bsize w, bsize h);
 #define IMG_RESOLUTION (IMG_WIDTH*IMG_HEIGHT)
 #define DIGIT_COUNT 10
 
-#define EPOCHS 100000
+#define EPOCHS 500000
 #define BATCH_SIZE (28*28)
 #define LEARNING_RATE 1e-1
+
+const char *db_paths[] = {
+    "examples/digit_recognition/database/train-images.idx3-ubyte",
+    "examples/digit_recognition/database/train-labels.idx1-ubyte",
+    "examples/digit_recognition/database/t10k-images.idx3-ubyte",
+    "examples/digit_recognition/database/t10k-labels.idx1-ubyte",
+};
 
 bsize arch[] = {IMG_RESOLUTION, 10, 28, 28, DIGIT_COUNT};
 bsize layers = sizeof (arch) / sizeof (arch[0]);
 
 int main(int argc, char **argv)
 {
-    if (argc != 5) {
-        PANIC("Usage: ./digit_recogniton <image training database> <label training database> <image evaluation database> <label evaluation database>");
+    if (argc != 2) {
+        PANIC("Usage: ./digit_recognition <nn_path>");
     }
 
-    Mat data = mnist_load(argv[1], argv[2], 1);
+    Mat data = mnist_load(db_paths[0], db_paths[1], 1);
 
     Mat tinput  = mat_sub_mat(data, 0, 0, data.r, IMG_RESOLUTION);
     Mat toutput = mat_sub_mat(data, 0, IMG_RESOLUTION, data.r, DIGIT_COUNT);
@@ -31,9 +38,10 @@ int main(int argc, char **argv)
     nn_init(nn, -1, 1);
     nn_set_activation_function(&nn, lReLU, dlReLU, Sigmoid, dSigmoid);
 
-    nn_ptrain(&nn, tinput, toutput, BATCH_SIZE, EPOCHS, LEARNING_RATE, 10, 1);
+    nn_ptrain(&nn, tinput, toutput, BATCH_SIZE, EPOCHS, LEARNING_RATE, 16, 1);
+    nn_save(argv[1], nn);
 
-    Mat eval   = mnist_load(argv[3], argv[4], 1);
+    Mat eval   = mnist_load(db_paths[2], db_paths[3], 1);
     Mat input  = mat_sub_mat(data, 0, 0, eval.r, IMG_RESOLUTION);
     Mat output = mat_sub_mat(data, 0, IMG_RESOLUTION, eval.r, DIGIT_COUNT);
 
